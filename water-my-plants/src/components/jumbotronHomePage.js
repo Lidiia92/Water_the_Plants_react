29bb0a7e420f2c5b49c330786e9a5478ef546d5d
Lidii <lidiiany92@gmail.com>
2019-02-07 12:17:08 -0500

    Added Headings to the jumbotron

--- water-my-plants/src/components/jumbotronHomePage.js
+++ water-my-plants/src/components/jumbotronHomePage.js
@@ -15,6 +15,7 @@ class Jumbotron extends Component {
            <NavigationMenu isLoggedIn={this.props.isLoggedIn} notify={this.props.notify} visitPage={this.props.visitPage}/>
            <div>
                <h2>Take Care of Your Plants</h2>
+               <p>Florem ipsum st john’s wort jersey lily canterbury bells sneezeweed love-in-a-mist. Crocus sword lily blue throatwort transvaal daisy arum.</p>
            </div>
        </WrapperCentered>
     );
