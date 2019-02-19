fbb08fa02e3e80db5ed664fbad2d3923a23857bb
Lidii <lidiiany92@gmail.com>
2019-02-19 15:57:02 -0500

    Updates

---
+++ precache-manifest.551dfc020f43fc2b29d324fa2c1e6cbc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a96820342a41ec14a13e733e74cd8c4a",
+    "url": "/Water_the_Plants_react/static/media/Capture01.a9682034.PNG"
+  },
+  {
+    "revision": "51ac4a0d6c3cfe9a8d31f4163102b6a2",
+    "url": "/Water_the_Plants_react/static/media/9290.51ac4a0d.jpg"
+  },
+  {
+    "revision": "b1f2f1d6130d3cd42637d64c03673877",
+    "url": "/Water_the_Plants_react/static/media/16181.b1f2f1d6.jpg"
+  },
+  {
+    "revision": "8a2f25b034d1a6507de5",
+    "url": "/Water_the_Plants_react/static/js/runtime~main.8a2f25b0.js"
+  },
+  {
+    "revision": "3f426d7b3887eb788d0c",
+    "url": "/Water_the_Plants_react/static/js/main.3f426d7b.chunk.js"
+  },
+  {
+    "revision": "aa9bd611947437020aee",
+    "url": "/Water_the_Plants_react/static/js/1.aa9bd611.chunk.js"
+  },
+  {
+    "revision": "3f426d7b3887eb788d0c",
+    "url": "/Water_the_Plants_react/static/css/main.d08be4a1.chunk.css"
+  },
+  {
+    "revision": "aa9bd611947437020aee",
+    "url": "/Water_the_Plants_react/static/css/1.98a670b9.chunk.css"
+  },
+  {
+    "revision": "9157e94947b6849306578147980f3a44",
+    "url": "/Water_the_Plants_react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Water_the_Plants_react/precache-manifest.551dfc020f43fc2b29d324fa2c1e6cbc.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Water_the_Plants_react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
