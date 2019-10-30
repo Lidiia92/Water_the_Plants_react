2cf4f80b64f9769a7aea9f10b1f51ed36aff9fa3
Lidii <lidiiany92@gmail.com>
2019-10-30 15:06:56 -0700

    navbar test

--- water-my-plants/src/components/__test__/NavBar.test.js
+++ water-my-plants/src/components/__test__/NavBar.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
+
 import NavBar from '../NavBar';
 
 it('renders without crashing', () => {
@@ -13,4 +14,11 @@ it('renders without crashing', () => {
 
 it('the navbarshould have signup button', () => {
     rtl.render(<NavBar><a>Sign Up</a></NavBar>);
+})
+
+it('signup button should have text "signup"', () => {
+
+    const { queryByText } = rtl.render(<NavBar></NavBar>)
+    expect(queryByText("Sign Up")).toBeTruthy(); 
+
 })
